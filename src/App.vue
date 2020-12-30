90436c0be18ddd9cbb6394cd6afa189ca2c22baf
DavidCaba6194 <cabayosoyasi@gmail.com>
2020-12-30 21:43:19 +0100

    Configuracion inicial y nuevas rutas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
+  <div id="nav">|
     <router-link to="/about">About</router-link>
+    <router-link to="/strava">Strava</router-link>
   </div>
   <router-view/>
 </template>
