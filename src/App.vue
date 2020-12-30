cc3d24da1d85240203128159521eafc7e1cdb260
DavidCaba6194 <cabayosoyasi@gmail.com>
2020-12-30 21:54:22 +0100

    Nueva página Home

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="nav">|
-    <router-link to="/about">About</router-link>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
     <router-link to="/strava">Strava</router-link>
   </div>
   <router-view/>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
