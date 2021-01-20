35ee32ba910110ab2d1a8161c6db3d2a3a74d2f1
DavidCaba6194 <cabayosoyasi@gmail.com>
2021-01-20 22:01:15 +0100

    BD mongo nube y crud completo con mongoose y morgan

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb+srv://david:temporal01@cluster0.4wdun.mongodb.net/pruebas?retryWrites=true&w=majority')
+    .then(db => console.log('BD conectada'))
+    .catch(err => console.error(err));
 
 //Settings
 app.set('port', 3000);
 
 //Middleware
+app.use(morgan('dev'));
+app.use(express.json());
 
 //Routes
+app.use('/tasks', require('./router/tasks'));
 
 //Static files
 app.use(express.static('localhost:8000/public'));
